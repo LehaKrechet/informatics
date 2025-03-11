d09cbd39306e15746740f416016513c6b0da9621
Alekres <alex072006@mail.ru>
2025-03-11 20:29:55 +0300

    11.03.25

--- massive/Rearrange_the_elements_in_reverse_order.cpp
+++ massive/Rearrange_the_elements_in_reverse_order.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 
 int main(){
-    int number, count = 0, elem, array[100] = {}, help_number;
+    int number, count = 0, start = 0, array[100] = {}, help_number;
     std::cin >> number;
-    help_number = number + 1;
+    help_number = number;
     for (int i = 0; i < number; ++i){
         std::cin >> array[i];
     }
-    for (int i = number-1; i >= 0; --i){
-        array[help_number] = array[i];
-        help_number += 1;
+    while (start < help_number) {
+        int temp = array[start];
+        array[start] = array[help_number];
+        array[help_number] = temp;
+        start++;
+        help_number--;
     }
-    help_number = 0;
-    for (int i = number+1; i < number+number+1; ++i){
-        array[help_number] = array[i];
-        array[i] = 0;
-        help_number += 1;
+    std::cout << array[1];
+    for (int i = 2; i <= number; ++i){
+        std::cout << " " << array[i];
     }
-    for (int i = 0; i < number; ++i){
-        std::cout << array[i] << " ";
-    }
+    std::cout << std::endl;
 }
